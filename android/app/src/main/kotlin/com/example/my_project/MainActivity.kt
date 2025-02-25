21a6e6643f0469cecdd4b66978565cf612869bd1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-25 18:23:08 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.healthintegrations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_code/actions/fetch_step_count_action.dart
@@ -0,0 +1,60 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:health/health.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<int> fetchStepCountAction() async {
+  // Add your function code here!// Global Health instance
+  final health = Health();
+  await health.configure();
+  await health.getHealthConnectSdkStatus();
+
+  // define the types to get
+  var types = [
+    HealthDataType.STEPS,
+  ];
+  var permissions = [HealthDataAccess.READ_WRITE];
+
+  bool authorized = await authorize(types, permissions);
+  if (authorized) {
+    debugPrint("User authorized");
+
+    var now = DateTime.now();
+    var midnight = DateTime(now.year, now.month, now.day);
+    int? steps = await health.getTotalStepsInInterval(midnight, now);
+    return steps ?? 0;
+  } else {
+    debugPrint("User not authorized");
+    return 0;
+  }
+}
+
+Future<bool> authorize(
+    List<HealthDataType> types, List<HealthDataAccess>? permissions) async {
+  await Permission.activityRecognition.request();
+  await Permission.location.request();
+
+  final health = Health();
+
+  bool? hasPermissions =
+      await health.hasPermissions(types, permissions: permissions);
+
+  hasPermissions = false;
+  bool authorized = false;
+  if (!hasPermissions) {
+    try {
+      authorized =
+          await health.requestAuthorization(types, permissions: permissions);
+    } catch (error) {
+      debugPrint("Exception in authorize: $error");
+      return false;
+    }
+  }
+  return authorized;
+}
